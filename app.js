1f399261903ebc376ee9954a2bb1019894bd8a75
lchyun23 <lchyun23@gmail.com>
2024-11-04 15:52:51 +0900

    [chore] undefined

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const amlIFat = null;
-// There's nothing here. 아무것도 없는 상태로 채워진 것.
-console.log(amlIFat);
+/* null은 variable 안에 어떤 것이 없다는 것을 확실히 하기 위해 쓰는 것이다.
+    즉, 존재하고 값이 주어졌는데, 그 값이 "비어있음" 인 것.
+*/
+let something;
+// 컴퓨터 메모리에는 존재하는데, 값이 들어있지 않음.
+console.log(something, amlIFat);
