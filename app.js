aeb585eb15b77ecfc91a91ce1ad8fa5422a9243d
lchyun23 <lchyun23@gmail.com>
2024-11-04 16:41:55 +0900

    [chore] arguments

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-// function : 계속 반복해서 사용할 수 있는 코드 조각
-console.log("Hello, my name is Nico");
-console.log("Hello, my name is Dal");
-console.log("Hello, my name is Shigatsu");
-console.log("Hello, my name is Flynn");
+// function : 계속 반복해서 사용할 수 있는 코드 조각. 같은 코드를 여러 번 반복해서 치지 않기 위해서 만든다.
+
+function sayHello(nameOfPerson, age) {
+  console.log("Hello, my name is " + nameOfPerson + "and I'm " + age);
+}
+
+// function 실행 : ()
+sayHello("nico", 19);
+sayHello("dal", 23);
+sayHello("lynn", 21);
