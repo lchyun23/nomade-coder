707bb9185c6a1c7afcb8a1d17f61f941108ead74
lchyun23 <lchyun23@gmail.com>
2024-11-04 15:42:54 +0900

    [chore] first training

---
+++ app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "nico";
+// let은 생성할 때 사용하고, 나중에는 변수명만 써서 변경 가능하다.
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "nicolas";
+console.log("your new name is " + myName);
