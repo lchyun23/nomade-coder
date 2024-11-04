ea8cf56666fd1694435e325c14fd15ca04477a4e
lchyun23 <lchyun23@gmail.com>
2024-11-04 15:47:04 +0900

    [chore] null

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const a = 5;
-const b = 2;
-let myName = "nico";
-// let은 생성할 때 사용하고, 나중에는 변수명만 써서 변경 가능하다.
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "nicolas";
-console.log("your new name is " + myName);
+const amlIFat = null;
+// There's nothing here. 아무것도 없는 상태로 채워진 것.
+console.log(amlIFat);
