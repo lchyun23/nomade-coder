108707ffaffa1612f51a347d1695d139ae3f6187
lchyun23 <lchyun23@gmail.com>
2024-11-04 17:24:06 +0900

    [chore] 계산기 만들었다.

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const player = {
-  name: "chany",
-  sayHello: function (otherPersonsName) {
-    console.log("hello! " + otherPersonsName + " nice to meet you");
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  powerof: function (a, b) {
+    console.log(a ** b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  multiply: function (a, b) {
+    console.log(a * b);
   },
 };
 
-console.log(player.name);
-player.sayHello("lynn");
-player.sayHello("nico");
-
-// function에게 정보를 보내는 법을 배웠다. console.log() 하는 것처럼 우리만의 것을 만들었다.
-
-function plus(a, b) {
-  console.log(a + b);
-}
-plus(5, 5);
+calculator.add(3, 4);
+calculator.divide(3, 4);
+calculator.minus(5, 6);
+calculator.multiply(2, 3);
+calculator.powerof(5, 6);
