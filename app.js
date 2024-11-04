fe3cea7a79eb302a4db1b61e018c80395ad2d1dd
lchyun23 <lchyun23@gmail.com>
2024-11-04 16:46:44 +0900

    [chore] function

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-// function : 계속 반복해서 사용할 수 있는 코드 조각. 같은 코드를 여러 번 반복해서 치지 않기 위해서 만든다.
-
-function sayHello(nameOfPerson, age) {
-  console.log("Hello, my name is " + nameOfPerson + "and I'm " + age);
+function plus(firstNumber, secondNumber) {
+  console.log(firstNumber + secondNumber);
 }
-
-// function 실행 : ()
-sayHello("nico", 19);
-sayHello("dal", 23);
-sayHello("lynn", 21);
+function divide(a, b) {
+  console.log(a / b);
+}
+plus(8, 60);
+divide(98, 20);
