b48f266c7cc01876243bfb0fd88127ac251fdae6
lchyun23 <lchyun23@gmail.com>
2024-11-04 15:56:12 +0900

    [chore] era without arrays

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const amlIFat = null;
-/* null은 variable 안에 어떤 것이 없다는 것을 확실히 하기 위해 쓰는 것이다.
-    즉, 존재하고 값이 주어졌는데, 그 값이 "비어있음" 인 것.
-*/
-let something;
-// 컴퓨터 메모리에는 존재하는데, 값이 들어있지 않음.
-console.log(something, amlIFat);
+// 자료구조 - 데이터의 저장 방법
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfWeek = mon + tue + wed + thu + fri + sat + sun;
+
+console.log(daysOfWeek);
